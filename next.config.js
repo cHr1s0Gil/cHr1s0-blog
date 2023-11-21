22ec57b08ba0e7bef68670542e43973501bacfba
Chriso004 <qudcksrlf1@gmail.com>
2023-11-21 10:02:26 +0900

    basePath 수정

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV = "production";
+
 const nextConfig = {
+    basePath: isProd ? "/cHr1s0-blog" : "",
     output: "export",
     images: { unoptimized: true }
 }
