33f874df600ec06d4f7799d0de4505dc7cc2c57a
Chriso004 <qudcksrlf1@gmail.com>
2023-11-21 17:21:20 +0900

    메인 배너 제작 완료

--- app/components/Profile.tsx
+++ app/components/Profile.tsx
@@ -5,10 +5,18 @@ import thumbStyles from "styles/components/Profile/ProfileThumb.module.css";
 import navStyles from "styles/components/Profile/ProfileNav.module.css";
 import infoStyles from "styles/components/Profile/ProfileInfo.module.css";
 
-import config from "../../next.config";
+import pathConfig from "../../next.config";
+
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faSquareInstagram, faVimeo } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import Link from "next/link";
+config.autoAddCss = false
 
 export default function ProfileInfo() {
-    const imagePath = config.basePath;
+    const imagePath = pathConfig.basePath;
 
     return (
         <div className={styles.profileContainer}>
@@ -31,15 +39,31 @@ export default function ProfileInfo() {
                 </div>
                 <div className={infoStyles.profileSubContainer}>
                     <span className={infoStyles.profileSub}>
-                        Introduce | Back
+                        Full-stack | Backend
                     </span>
                 </div>
             </div>
             <div className={navStyles.profileNavContainer}>
-                <div className={navStyles.githubContainer}></div>
-                <div className={navStyles.velogContainer}></div>
-                <div className={navStyles.instagramContainer}></div>
-                <div className={navStyles.mailContainer}></div>
+                <Link className={navStyles.linkContainer} href={"https://github.com/cHr1s0326"} target="_blank">
+                    <div className={navStyles.githubContainer}>
+                        <FontAwesomeIcon icon={faGithub} size="3x" />
+                    </div>
+                </Link>
+                <Link className={navStyles.linkContainer} href={"https://velog.io/@chr1s0"} target="_blank">
+                    <div className={navStyles.velogContainer}>
+                        <FontAwesomeIcon icon={faVimeo} size="3x" />
+                    </div>
+                </Link>
+                <Link className={navStyles.linkContainer} href={"https://www.instagram.com/ch_r1s0/"} target="_blank">
+                <div className={navStyles.instagramContainer}>
+                    <FontAwesomeIcon icon={faSquareInstagram} size="3x" />
+                </div>
+            </Link>
+            <Link className={navStyles.linkContainer} href={"mailto:qudcksrlf1@gmail.com"}>
+                <div className={navStyles.mailContainer}>
+                    <FontAwesomeIcon icon={faEnvelope} size="3x" />
+                </div>
+            </Link>
             </div>
         </div>
     )
