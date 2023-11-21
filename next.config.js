746b5e0dab2db5b97939bc817814b5452f617485
Chriso004 <qudcksrlf1@gmail.com>
2023-11-21 12:41:49 +0900

    config 수정

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const isProd = process.env.NODE_ENV = "production";
 const nextConfig = {
     // basePath: isProd ? "/cHr1s0-blog" : "",
     output: "export",
+    images: { unoptimized: true }
 };
 
 
