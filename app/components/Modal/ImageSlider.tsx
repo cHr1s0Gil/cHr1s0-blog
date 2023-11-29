3ae7ed21486539e1339a31427f2e322bc8f0e9f5
Chriso004 <qudcksrlf1@gmail.com>
2023-11-29 17:56:03 +0900

    이미지 슬라이더 수정중

---
+++ app/components/Modal/ImageSlider.tsx
@@ -0,0 +1,45 @@
+
+import Image from "next/image";
+import { Dispatch, RefObject, SetStateAction, useEffect, useRef, useState } from "react";
+
+import styles from "styles/components/Modal/ImageSlider.module.css";
+
+export default function ImageSlider() {
+    const IMAGE_SIZE: number = 5;
+    const images: number[] = Array.from({ length: IMAGE_SIZE }, (v: unknown, index: number) => index + 1);
+
+    const sliderRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+    const [currentIndex, setCurrentIndex]: [number, Dispatch<SetStateAction<number>>] = useState<number>(0);
+
+    const nextSlide = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % IMAGE_SIZE);
+    }
+
+    const prevSlide = () => {
+        setCurrentIndex((prevIndex) => (prevIndex - 1 + IMAGE_SIZE) % IMAGE_SIZE);
+    }
+
+    return (
+        <div className={styles.sliderContainer}>
+            <div ref={sliderRef} className={styles.imageContainer}>
+                {images.map((value: number, index: number) => (
+                    <Image
+                        key={index}
+                        className={styles.image}
+                        // className={`${styles.image} ${value === currentIndex + 1 ? styles.active : ''}`}
+                        src={`static/images/test/image_${value}.png`}
+                        alt={`이미지 ${index}`}
+                        width={1920}
+                        height={1080}
+                        >
+                    </Image>
+                ))}
+            </div>
+            <div className={styles.slideBtnContainer}>
+                <div className={styles.prevBtn} onClick={prevSlide}>{"<"}</div>
+                <span className={styles.pagination}>{currentIndex + 1}/{IMAGE_SIZE}</span>
+                <div className={styles.nextBtn} onClick={nextSlide}>{">"}</div>
+            </div>
+        </div>
+    );
+}
--- app/components/Modal/Modal.tsx
+++ app/components/Modal/Modal.tsx
@@ -1,19 +1,40 @@
+import Image from "next/image";
+
 import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import styles from "styles/components/Modal/Modal.module.css";
+import headerStyles from "styles/components/Modal/ModalHeader.module.css";
+import contentStyles from "styles/components/Modal/ModalContent.module.css";
+import ImageSlider from "./ImageSlider";
 
 export default function Modal({ props }: { props: IModalProps }) {
     const { index, onClose }: IModalProps = props;
 
+    const onModalClick = (event: React.MouseEvent<HTMLDivElement>) => {
+        const modal: Element | null = document.querySelector(".modalContainer");
+        
+        if(modal && modal === event.target)
+            onClose();
+    }
+
     return (
-        <div className={styles.modalContainer}>
+        <div className={`${styles.modalContainer} modalContainer`} onClick={onModalClick}>
             <div className={styles.modalContent}>
-                <div className={styles.titleContainer}>
-                    <span className={styles.title}>Title</span>
-                    <div className={styles.closeContainer}>
-                        <div className={styles.closeBtn} onClick={onClose}>
+                <div className={headerStyles.titleContainer}>
+                    <span className={headerStyles.title}>Title</span>
+                    <div className={headerStyles.closeContainer}>
+                        <div className={headerStyles.closeBtn} onClick={onClose}>
                             <FontAwesomeIcon icon={faCircleXmark} size="3x" />
                         </div>
+                    </div>
+                </div>
+                <div className={headerStyles.subTitleContainer}>
+                    <span className={headerStyles.subTitle}>subTitle</span>
+                </div>
+                <div className={contentStyles.modalContentContainer}>
+                    <ImageSlider></ImageSlider>
+                    <div className={contentStyles.descriptionContainer}>
                     </div>
                 </div>
             </div>
